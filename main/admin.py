96954faef10c82328ede96f89b93f6c7df4e7b41
Aboubakr <abobkrahmed99@gmail.com>
2026-01-20 08:29:28 +0200

    fixed logging mechanism

--- main/admin.py
+++ main/admin.py
@@ -3,16 +3,30 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import *
 
 class UserAdmin(BaseUserAdmin):
-    # Add custom fields to fieldsets
-    fieldsets = BaseUserAdmin.fieldsets + (
+    # Remove first_name and last_name from fieldsets as they don't exist in our User model
+    # base_fieldsets is usually:
+    # (
+    #   (None, {'fields': ('username', 'password')}), 
+    #   ('Personal info', {'fields': ('first_name', 'last_name', 'email')}), 
+    #   ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}), 
+    #   ('Important dates', {'fields': ('last_login', 'date_joined')})
+    # )
+    
+    # We define our own fieldsets
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('full_name', 'email')}), # Replaced first_name/last_name with full_name
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
         ('Custom Fields', {'fields': ('primary_role', 'department', 'gender', 'profile_picture_url')}),
         ('Student Info', {'fields': ('student_id', 'student_current_level', 'current_gpa', 'current_streak', 'longest_streak')}),
     )
-    # Add custom fields to list_display
-    list_display = BaseUserAdmin.list_display + ('primary_role', 'department')
+    
+    # Also fix list_display to use full_name instead of first_name/last_name
+    list_display = ('username', 'email', 'full_name', 'primary_role', 'department', 'is_staff')
     
     # Allow searching by custom fields
-    search_fields = BaseUserAdmin.search_fields + ('student_id',)
+    search_fields = ('username', 'full_name', 'email', 'student_id')
 
 # Register your models here.
 admin.site.register(User, UserAdmin)
