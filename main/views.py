6324522e6ec0a4916cfba49505f5765870ff3df2
Aboubakr <abobkrahmed99@gmail.com>
2026-05-07 23:40:12 +0300

    implemented streaks

--- main/views.py
+++ main/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.response import Response
+from rest_framework import status
+from django.utils import timezone
+from datetime import timedelta
 from .serializers import EmailTokenObtainPairSerializer
-
-# Create your views here.
 
 
 class EmailTokenObtainPairView(TokenObtainPairView):
@@ -10,3 +12,34 @@ class EmailTokenObtainPairView(TokenObtainPairView):
     Custom token obtain view that uses email instead of username for authentication.
     """
     serializer_class = EmailTokenObtainPairSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        try:
+            serializer.is_valid(raise_exception=True)
+        except Exception:
+            return Response(serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
+
+        user = serializer.user
+        self._update_streak(user)
+
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
+
+    def _update_streak(self, user):
+        now = timezone.now()
+        if user.last_login is None:
+            user.current_streak = 1
+        else:
+            time_since = now - user.last_login
+            if time_since < timedelta(hours=24):
+                return
+            elif time_since < timedelta(hours=48):
+                user.current_streak += 1
+            else:
+                user.current_streak = 1
+
+        if user.current_streak > user.longest_streak:
+            user.longest_streak = user.current_streak
+
+        user.last_login = now
+        user.save(update_fields=['current_streak', 'longest_streak', 'last_login'])
