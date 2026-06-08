13dd6962cb8149ab912f8fd590b77c65d35b0647
Aboubakr <abobkrahmed99@gmail.com>
2026-06-08 20:21:46 +0300

    Fix pymysql datetime field conversion

--- GraduationProject/__init__.py
+++ GraduationProject/__init__.py
@@ -1,2 +1,12 @@
 import pymysql
+
+from pymysql.constants import FIELD_TYPE
+from pymysql.converters import conversions
+import datetime
+
+# Ensure MySQL DATETIME/TIMESTAMP fields are returned as datetime objects
+conversions[FIELD_TYPE.DATETIME] = datetime.datetime
+conversions[FIELD_TYPE.DATE] = datetime.date
+conversions[FIELD_TYPE.TIMESTAMP] = datetime.datetime
+
 pymysql.install_as_MySQLdb()
