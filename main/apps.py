04a7a7fb1a302dbf2828a92f9add11b1e5a591bb
Aboubakr <abobkrahmed99@gmail.com>
2026-06-08 23:53:24 +0300

    Fix: only patch convert_datetimefield_value (other converters don't exist in Django 5.2)

--- main/apps.py
+++ main/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
-from django.utils.dateparse import parse_datetime, parse_date, parse_time
+from django.utils.dateparse import parse_datetime
 
 
 class MainConfig(AppConfig):
@@ -7,39 +7,15 @@ class MainConfig(AppConfig):
     name = 'main'
 
     def ready(self):
-        # Monkey-patch Django MySQL backend to handle string datetime values
-        # returned by pymysql when mysqlclient is not available
         from django.db.backends.mysql.operations import DatabaseOperations
 
-        orig_convert_datetimefield_value = DatabaseOperations.convert_datetimefield_value
+        orig_func = DatabaseOperations.convert_datetimefield_value
 
         def convert_datetimefield_value(self, value, expression, connection):
             if isinstance(value, str):
                 parsed = parse_datetime(value)
                 if parsed is not None:
                     value = parsed
-            return orig_convert_datetimefield_value(self, value, expression, connection)
+            return orig_func(self, value, expression, connection)
 
         DatabaseOperations.convert_datetimefield_value = convert_datetimefield_value
-
-        orig_convert_datefield_value = DatabaseOperations.convert_datefield_value
-
-        def convert_datefield_value(self, value, expression, connection):
-            if isinstance(value, str):
-                parsed = parse_date(value)
-                if parsed is not None:
-                    value = parsed
-            return orig_convert_datefield_value(self, value, expression, connection)
-
-        DatabaseOperations.convert_datefield_value = convert_datefield_value
-
-        orig_convert_timefield_value = DatabaseOperations.convert_timefield_value
-
-        def convert_timefield_value(self, value, expression, connection):
-            if isinstance(value, str):
-                parsed = parse_time(value)
-                if parsed is not None:
-                    value = parsed
-            return orig_convert_timefield_value(self, value, expression, connection)
-
-        DatabaseOperations.convert_timefield_value = convert_timefield_value
